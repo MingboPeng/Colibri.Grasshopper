32cf2ce228b477c62fde31216ac641954686c3e8
Grafenberg <olivierdambron@hotmail.com>
2016-12-10 15:49:42 +0000

    updateicons

--- src/Colibri.Grasshopper/Properties/Resources.Designer.cs
+++ src/Colibri.Grasshopper/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace Colibri.Grasshopper.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Colibri_logobase_5 {
+            get {
+                object obj = ResourceManager.GetObject("Colibri_logobase_5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
