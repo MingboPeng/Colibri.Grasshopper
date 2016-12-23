7d208a1b3d8596818f537493895278278f0ac792
Mingbo Peng <MPeng@thorntontomasetti.com>
2016-12-23 14:46:50 -0500

    first commit of Iterator2
    Iterator takes both slider and panel

---
+++ src/Colibri.Grasshopper/Iterator2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using GH = Grasshopper;
+using Grasshopper.Kernel.Special;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Linq;
+
+namespace Colibri.Grasshopper
+{
+    public class Iterator2 : GH_Component
+    {
+        
+        GH_Document doc = null;
+        public List<GH_NumberSlider> allConnectedSliders = new List<GH_NumberSlider>();
+
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public Iterator2()
+          : base("Iterator2", "Iterator",
+              "Description",
+              "Colibri", "Colibri")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("inputs", "inputs", "Slider or Panel", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Fly?", "Fly?", "Tell Colibri to fly!  Provide a button here, and click it once you are ready for Colibri to fly around your definition.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("input", "input", "current item of inputs", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            if (doc == null)
+            {
+                doc = GH.Instances.ActiveCanvas.Document;
+            }
+
+            //List<string> inputs = new List<string>();
+            //DA.GetDataList(0, inputs);
+
+            var sliderAndPanel = new IteratorGetSliderPanel();
+            var selectedSliderAndPanel = sliderAndPanel.getConnectedSliderOrPanel(this.Params.Input[0]);
+
+            DA.SetDataList(0, selectedSliderAndPanel);
+
+        }
+
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{74a79561-b3b2-4e12-beb4-d79ec0ed378a}"); }
+        }
+    }
+}
