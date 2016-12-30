16d94680396d1f6aded599e62af85bb049a8e6fe
Mingbo Peng <mingo1214@gmail.com>
2016-12-30 09:18:48 -0500

    Commit from GitHub desktop test
    test

--- src/Colibri.Grasshopper/IteratorParam.cs
+++ src/Colibri.Grasshopper/IteratorParam.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Colibri.Grasshopper;
 using Grasshopper.Kernel.Types;
 using Grasshopper.Kernel.Parameters;
 
